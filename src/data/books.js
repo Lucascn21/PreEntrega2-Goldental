e3bf65419a741d4cd9f54742d78a353c9ae13b0b
lucascn21 <lucascn21@gmail.com>
2023-01-23 14:21:59 -0300

    prop naming cleanup

--- src/data/books.js
+++ src/data/books.js
@@ -1,1 +1,1 @@
-module.exports = [{ id: 1, bookName: "Test", price: 999, productImage: "asd" },{ id: 2, bookName: "Test", price: 999, productImage: "asd" }];
+module.exports = [{ id: 1, bookName: "Test", price: 999, bookImage: "asd" },{ id: 2, bookName: "Test", price: 999, bookImage: "asd" }];
