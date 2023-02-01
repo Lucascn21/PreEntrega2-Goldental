8e8c2c89ecd01b266444f9f2e8fca29822373961
lucascn21 <lucascn21@gmail.com>
2023-02-01 14:57:57 -0300

    mocking ItemPage

--- src/pages/itempage/ItemPage.jsx
+++ src/pages/itempage/ItemPage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { ItemDetailContainer, ItemCount } from "../../components";
+import { ItemDetailContainer } from "../../components";
 import { useParams } from "react-router-dom";
 import Container from "@mui/material/Container";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2";
-import "./ItemPage.scss";
+
 const ItemMUI = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
@@ -17,8 +17,14 @@ export default function ItemPage(props) {
   const { itemId } = useParams();
   return (
     <Container maxWidth="sm">
-      <Grid component="main" container spacing={1} xs="auto">
-        <Grid xs={8}>
+      <Grid
+        sx={{ alignItems: "center", justifyContent: "center" }}
+        component="main"
+        container
+        spacing={1}
+        xs="auto"
+      >
+        <Grid xs={12}>
           <ItemMUI>
             <ItemDetailContainer itemIdValue={itemId} />
           </ItemMUI>
