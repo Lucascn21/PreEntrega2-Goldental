70b8a6bf4d43e93a341c2f9aeff9b08ee6af9242
lucascn21 <lucascn21@gmail.com>
2023-02-01 14:08:56 -0300

    forgot to parse itemId on getItem, code cleanup

--- src/components/ItemDetail/ItemDetailContainer.jsx
+++ src/components/ItemDetail/ItemDetailContainer.jsx
@@ -1,22 +1,24 @@
 import { useState, useEffect } from "react";
 import ItemDetail from "../ItemDetail/ItemDetail";
 
-export default function ItemDetailContainer({ itemData }) {
+export default function ItemDetailContainer({ itemIdValue }) {
   const [item, setItem] = useState([]);
   useEffect(() => {
-    getItem(setItem, itemData.id);
-  }, [itemData.id]);
-  return <ItemDetail itemData={item} />;
+    getItem(setItem, itemIdValue);
+  }, [itemIdValue]);
+  return <ItemDetail itemData={item}></ItemDetail>;
 }
 
-const getItem = (setItem, itemDataId) => {
+const getItem = async (setItem, itemId) => {
   fetch("../json/books.json")
     .then((response) => response.json())
     .then((items) => {
       setTimeout(() => {
-        const selectedItem = items.find((item) => item.id === itemDataId);
+        const selectedItem = items.find(
+          (item) => item.itemId === Number.parseInt(itemId)
+        );
         setItem(selectedItem);
-        console.log("Delayed for 2 seconds.");
+        console.log("Delayed individual item detail fetching for 2 seconds.");
       }, 2000);
     });
 };
--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -4,7 +4,7 @@ import Grid from "@mui/material/Unstable_Grid2";
 export default function Shop(props) {
   return (
     <Grid  component="main" container spacing={1} xs="auto">
-      <ItemListcontainer pageIndex={props.pageIndex}></ItemListcontainer>
+      <ItemListcontainer></ItemListcontainer>
     </Grid>
   );
 }
