a32f9b4334211713bb4da5162cbcfb5933c85459
lucascn21 <lucascn21@gmail.com>
2023-02-01 14:35:34 -0300

    Mocking item page

--- src/pages/itempage/ItemPage.jsx
+++ src/pages/itempage/ItemPage.jsx
@@ -1,12 +1,29 @@
 import React from "react";
-import { ItemDetailContainer } from "../../components";
+import { ItemDetailContainer, ItemCount } from "../../components";
 import { useParams } from "react-router-dom";
+import Container from "@mui/material/Container";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2";
-export default function ItemPage() {
+import "./ItemPage.scss";
+const ItemMUI = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  justifyContent: "center",
+}));
+export default function ItemPage(props) {
   const { itemId } = useParams();
   return (
-    <Grid component="main" container spacing={1} xs="auto">
-      <ItemDetailContainer itemIdValue={itemId} />
-    </Grid>
+    <Container maxWidth="sm">
+      <Grid component="main" container spacing={1} xs="auto">
+        <Grid xs={8}>
+          <ItemMUI>
+            <ItemDetailContainer itemIdValue={itemId} />
+          </ItemMUI>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
