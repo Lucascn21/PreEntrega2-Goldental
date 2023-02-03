10b3c8132a6fdd9f4560ce77a38afc47cad8076e
lucascn21 <lucascn21@gmail.com>
2023-02-03 18:59:17 -0300

    removed unnecesary map, oops

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import * as React from "react";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import {pages} from "./data/pages";
 
 export default function App() {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
@@ -29,18 +28,12 @@ export default function App() {
           <NavBar />
           <Routes>
             <Route path="/" to="/Home" element={<Shop />}></Route>
-            {pages.map((page) => (
-              <Route
-                key={page}
-                path={`category/:categoryId`}
-                element={<Shop />}
-              ></Route>
-            ))}
 
             <Route path="/cart" element={<Cart />}></Route>
             <Route path="/mission" element={<Mission />}></Route>
             <Route path="/contact" element={<Contact />}></Route>
             <Route path="/item/:itemId" element={<ItemPage />}></Route>
+            <Route path={`category/:categoryId`} element={<Shop />}></Route>
           </Routes>
         </Router>
       </>
