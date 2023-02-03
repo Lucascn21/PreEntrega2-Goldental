3f2721487e161a0c179c8ad48638efe982ac97d1
lucascn21 <lucascn21@gmail.com>
2023-02-03 20:48:18 -0300

    hiding X overflow

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "./App.scss"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { NavBar } from "./components/NavBar/NavBar";
 import { Shop, Cart, Mission, Contact, ItemPage } from "./pages";
