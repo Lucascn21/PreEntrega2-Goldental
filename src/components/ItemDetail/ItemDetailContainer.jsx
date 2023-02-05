8fe6502fc49b2867aafe023ef7a8e10c15a4deb0
lucascn21 <lucascn21@gmail.com>
2023-02-05 09:58:11 -0300

    Navigation redirects if category or item not found

--- src/components/ItemDetail/ItemDetailContainer.jsx
+++ src/components/ItemDetail/ItemDetailContainer.jsx
@@ -1,15 +1,16 @@
 import { useState, useEffect } from "react";
 import { ItemDetail } from "../ItemDetail/ItemDetail";
-
+import { useNavigate } from "react-router-dom";
 export const ItemDetailContainer = ({ itemIdValue }) => {
+  let navigate = useNavigate();
   const [item, setItem] = useState([]);
   useEffect(() => {
-    getItem(setItem, itemIdValue);
-  }, [itemIdValue]);
+    getItem(setItem, itemIdValue, navigate);
+  }, [itemIdValue, navigate]);
   return <ItemDetail itemData={item}></ItemDetail>;
 };
 
-const getItem = async (setItem, itemId) => {
+const getItem = async (setItem, itemId, navigate) => {
   fetch("../json/books.json")
     .then((response) => response.json())
     .then((items) => {
@@ -17,6 +18,7 @@ const getItem = async (setItem, itemId) => {
         const selectedItem = items.find(
           (item) => item.itemId === Number.parseInt(itemId)
         );
+        if (!selectedItem) navigate("/not-found");
         setItem(selectedItem);
         console.log("Delayed individual item detail fetching for 2 seconds.");
       }, 2000);
--- src/components/ItemList/ItemListcontainer.jsx
+++ src/components/ItemList/ItemListcontainer.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import "./ItemListContainer.scss";
 import { ItemList } from "./ItemList";
 import { useParams } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
+import { pages } from "../../data/pages";
 /*
 Container component that Receives a Shop or Cart Component as a children to render
 This component will be used as a Container Component in Shop and Cart.
@@ -10,9 +11,10 @@ This component will be used as a Container Component in Shop and Cart.
 export const ItemListcontainer = () => {
   const { categoryId } = useParams();
   const [items, setItems] = useState([]);
+  let navigate = useNavigate();
   useEffect(() => {
-    getItems(setItems);
-  }, [categoryId]);
+    getItems(setItems, categoryId, navigate);
+  }, [categoryId, navigate]);
 
   const filteredBooks = filteredItemList(items, categoryId);
   return <ItemList filteredItems={filteredBooks} />;
@@ -20,17 +22,27 @@ export const ItemListcontainer = () => {
 
 //Function that returns a filtered item array based on its corresponding page section
 const filteredItemList = (items, currentRoute) => {
-  const filtereditems = items.filter((item) => item.itemSection === currentRoute);
+  const filtereditems = items.filter(
+    (item) => item.itemSection === currentRoute
+  );
+
   return filtereditems.length ? filtereditems : items;
 };
 
-const getItems = (setItems) => {
+const getItems = (setItems, categoryId, navigate) => {
   fetch("../json/books.json")
     .then((response) => response.json())
     .then((items) => {
       setTimeout(() => {
-        setItems(items);
-        console.log("Delayed item list fetching for 2 seconds.");
+        //Redirect if Shop route is not valid
+        const existingRoutes = new Set(pages);
+        if (existingRoutes.has(categoryId)) {
+          setItems(items);
+          console.log("Delayed item list fetching for 2 seconds.");
+        } else {
+          setItems(items);
+          navigate("/");
+        }
       }, 2000);
     });
 };
