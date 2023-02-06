a25bf136aa9c5fe5ffdd6bc273d402b89a7eecf6
lucascn21 <lucascn21@gmail.com>
2023-02-06 18:25:52 -0300

    installed react-toastify

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import * as React from "react";
 import "./App.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { NavBar } from "./components/NavBar/NavBar";
 import { Shop, Cart, Mission, Contact, ItemPage, NotFound } from "./pages";
-
-import * as React from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -24,20 +25,31 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <>
-        <Router>
-          <NavBar />
-          <Routes>
-            <Route path="/" to="/home" element={<Shop />}></Route>
-            <Route path="/cart" element={<Cart />}></Route>
-            <Route path="/mission" element={<Mission />}></Route>
-            <Route path="/contact" element={<Contact />}></Route>
-            <Route path="/item/:itemId" element={<ItemPage />}></Route>
-            <Route path="category/:categoryId" element={<Shop />}></Route>
-            <Route path="/not-found" element={<NotFound />}></Route>
-          </Routes>
-        </Router>
-      </>
+
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" to="/home" element={<Shop />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/mission" element={<Mission />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/item/:itemId" element={<ItemPage />}></Route>
+          <Route path="category/:categoryId" element={<Shop />}></Route>
+          <Route path="/not-found" element={<NotFound />}></Route>
+        </Routes>
+      </Router>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </ThemeProvider>
   );
 }
