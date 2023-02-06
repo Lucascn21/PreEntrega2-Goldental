91493359af955b3bd7bff5dc1e94ca1aa785e709
lucascn21 <lucascn21@gmail.com>
2023-02-06 17:52:59 -0300

    shop mock styling

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -3,7 +3,7 @@ import { ItemListcontainer } from "../../components/ItemList/ItemListcontainer";
 import Grid from "@mui/material/Unstable_Grid2";
 export const Shop = () => {
   return (
-    <Grid component="main" container spacing={1} xs="auto">
+    <Grid id="shop-main" component="main" container spacing={1} xs="auto">
       <ItemListcontainer />
     </Grid>
   );
