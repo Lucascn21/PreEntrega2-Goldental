6bc6f74309d19a3d92ac934baf3d23e6a25db9d2
lucascn21 <lucascn21@gmail.com>
2023-02-07 15:35:47 -0300

    some comments

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -7,14 +7,18 @@ import { Link as RouterLink } from "react-router-dom";
 import { ItemCount } from "../ItemCount/ItemCount";
 import { Button } from "@mui/material";
 import { toast } from "react-toastify";
+
 export const ItemDetail = (props) => {
+  //Props
   const { itemName, price, stock, itemSection, itemImage, itemDescription } =
     props.itemData;
 
+  //Img Placeholding
   const imgFoundPath =
     process.env.PUBLIC_URL + `/img/${itemSection}/${itemImage}`;
   const imgNotFoundPath = process.env.PUBLIC_URL + `/img/no_image.jpg`;
 
+  //Component helpers
   const isStocked = (stock) => stock < 1;
 
   return (
