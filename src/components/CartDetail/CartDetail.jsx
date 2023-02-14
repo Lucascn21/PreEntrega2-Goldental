c5ce9dcfe89997c439cd02418aae55baf5bd132f
lucascn21 <lucascn21@gmail.com>
2023-02-14 15:10:45 -0300

    CartDetail now renders childres only

--- src/components/CartDetail/CartDetail.jsx
+++ src/components/CartDetail/CartDetail.jsx
@@ -1,19 +1,5 @@
 import * as React from "react";
 
-import { Container } from "@mui/system";
-import { cartItems } from "../../data/carItems";
-import { ItemDetail } from "../ItemDetail/ItemDetail";
-
-//Get Columns
-const columnsValues = Object.keys(cartItems[0]);
-for (const column of columnsValues) {
-  console.dir(column);
-}
-
-export function CartDetail() {
-  return (
-    <Container>
-      <ItemDetail itemData={cartItems[0]}></ItemDetail>
-    </Container>
-  );
+export function CartDetail(props) {
+  return <div>{props.children}</div>;
 }
