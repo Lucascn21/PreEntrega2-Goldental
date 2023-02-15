4819b4e8cdb2a14806bbba2fc224171afd1a7b82
lucascn21 <lucascn21@gmail.com>
2023-02-15 08:20:43 -0300

    Text if cart is empty

--- src/components/CartDetail/CartDetail.jsx
+++ src/components/CartDetail/CartDetail.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 
 export function CartDetail(props) {
-  return <div>{props.children}</div>;
+  return <main>{props.children}</main>;
 }
--- src/components/CartDetail/CartDetailContainer.jsx
+++ src/components/CartDetail/CartDetailContainer.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 import { CartDetail } from "../CartDetail/CartDetail";
 import { CartItemList } from "../CartDetail/CartItemList";
-import { useNavigate } from "react-router-dom";
 import { useCartContext } from "../../context/CartContext";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
 export const CartDetailContainer = () => {
   const cartContext = useCartContext();
   const [cartItemsMap, setcartItemsMap] = useState(cartContext.cart);
@@ -10,21 +13,42 @@ export const CartDetailContainer = () => {
   let elements = [];
   for (const [item, quantity] of [...cartItemsMap]) {
     elements.push({ quantity, item });
-
   }
-  let navigate = useNavigate();
   useEffect(() => {
-    getItemsInCart(setcartItemsMap, cartItemsMap, navigate);
-  }, [cartItemsMap, navigate]);
+    getItemsInCart(setcartItemsMap, cartItemsMap);
+  }, [cartItemsMap]);
   return (
     <CartDetail>
-      <CartItemList>{elements.map((product) => product)}</CartItemList>
+      {!cartItemsMap.size ? (
+        <Container maxWidth="sm">
+          <Box sx={{ marginTop: "2rem", height: "100vh" }}>
+            <Typography variant="h3" gutterBottom>
+              Your cart is empty!
+            </Typography>
+            <Typography variant="subtitle1" gutterBottom>
+              Lets go back and find some great books for you to read.
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              Books are the best! we can’t imagine a life without reading. Not
+              only is reading imperative in this day and age, but it’s also
+              entertaining. We hope our passion for books shows and helps you on
+              your journey to a reading adventure.
+            </Typography>
+            <Typography variant="overline" display="block" gutterBottom>
+              <Link href="/" underline="hover" color="primary">
+                {"Back to shopping"}
+              </Link>
+            </Typography>
+          </Box>
+        </Container>
+      ) : (
+        <CartItemList>{elements.map((product) => product)}</CartItemList>
+      )}
     </CartDetail>
   );
 };
 
 //Todo either navigate or render something else
-const getItemsInCart = (setItem, cartItemsMap, navigate) => {
-  if (!cartItemsMap) navigate("/not-found");
+const getItemsInCart = (setItem, cartItemsMap) => {
   setItem(cartItemsMap);
 };
