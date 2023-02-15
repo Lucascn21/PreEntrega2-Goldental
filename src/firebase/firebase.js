dae49a61a995b477f046bef677d1af4770497f2c
lucascn21 <lucascn21@gmail.com>
2023-02-15 16:58:41 -0300

    starting with firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBB5QEL4KX-NXf8H8kJPR8Pl_Vg3KWZv48",
+  authDomain: "react-coder-lgm.firebaseapp.com",
+  projectId: "react-coder-lgm",
+  storageBucket: "react-coder-lgm.appspot.com",
+  messagingSenderId: "433780924976",
+  appId: "1:433780924976:web:234da603b63c77215ac3f5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
