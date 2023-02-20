7aacd3abf717f1c079834e2b4260856bd5e778e3
lucascn21 <lucascn21@gmail.com>
2023-02-20 10:45:19 -0300

    Gets products by name and id, validates route

--- src/components/ItemDetail/ItemDetailContainer.jsx
+++ src/components/ItemDetail/ItemDetailContainer.jsx
@@ -1,15 +1,28 @@
 import { useState, useEffect } from "react";
 import { ItemDetail } from "../ItemDetail/ItemDetail";
 import { useNavigate } from "react-router-dom";
-import { getProduct } from "../../firebase/firebase";
-export const ItemDetailContainer = ({ itemIdValue }) => {
+import { getProductById, getProductByName } from "../../firebase/firebase";
+export const ItemDetailContainer = (props) => {
   let navigate = useNavigate();
   const [item, setItem] = useState([]);
+  const isRouteValid = (item, route) => {
+    return route === `${item.itemName}-${item.itemSection}`.toLowerCase();
+  };
   useEffect(() => {
-    getProduct(itemIdValue).then((item) => {
-      if (!item) navigate("/not-found");
-      setItem(item.book);
-    });
-  }, [itemIdValue, navigate]);
+    if (props.itemID) {
+      getProductById(props.itemID).then((item) => {
+        if (!item) navigate("/not-found");
+        setItem(item);
+      });
+    } else if (props.itemIdRouteValue) {
+      getProductByName(props.itemIdRouteValue).then((item) => {
+        if (isRouteValid(item, props.itemIdRouteValue)) {
+          setItem(item);
+        } else {
+          navigate("/not-found");
+        }
+      });
+    }
+  }, [props, navigate]);
   return <ItemDetail itemData={item}></ItemDetail>;
 };
