5e892c4b1600d7a44c12833c5343bcf2e97d815e
lucascn21 <lucascn21@gmail.com>
2023-02-21 10:51:21 -0300

    more CartItemList Mocking

--- src/components/CartDetail/CartItemList.jsx
+++ src/components/CartDetail/CartItemList.jsx
@@ -9,6 +9,7 @@ import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import { useCartContext } from "../../context/CartContext";
 import { toast } from "react-toastify";
+import { Checkout } from "../Checkout/Checkout";
 export const CartItemList = (props) => {
   const cartContext = useCartContext();
   const rows = [];
@@ -37,61 +38,89 @@ export const CartItemList = (props) => {
   }
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Image</TableCell>
-            <TableCell align="right">Unit Price&nbsp;($)</TableCell>
-            <TableCell align="right">Total Price&nbsp;($)</TableCell>
-            <TableCell align="right">Type</TableCell>
-            <TableCell align="right">Quantity</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.itemName}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.itemName}
-              </TableCell>
-              <TableCell align="right">{row.itemImage}</TableCell>
-              <TableCell align="right">&nbsp;${new Intl.NumberFormat("de-DE").format(row.price)}</TableCell>
-              <TableCell align="right">&nbsp;${new Intl.NumberFormat("de-DE").format(row.price*row.quantity)}</TableCell>
-              <TableCell align="right">{row.itemSection}</TableCell>
-              <TableCell align="right">{row.quantity}</TableCell>
+    <>
+      <TableContainer sx={{ marginTop: "2rem" }} component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Image</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Unit Price</TableCell>
+              <TableCell>Total Price</TableCell>
+              <TableCell>Type</TableCell>
+              <TableCell>Quantity</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.itemName}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>
+                  <img
+                    width={"100rem"}
+                    height={"100rem"}
+                    src={row.itemImage}
+                    alt={row.itemName}
+                  />
+                </TableCell>
+                <TableCell component="th">{row.itemName}</TableCell>
+                <TableCell>
+                  &nbsp;${new Intl.NumberFormat("de-DE").format(row.price)}
+                </TableCell>
+                <TableCell>
+                  &nbsp;$
+                  {new Intl.NumberFormat("de-DE").format(
+                    row.price * row.quantity
+                  )}
+                </TableCell>
+                <TableCell>{row.itemSection}</TableCell>
+                <TableCell>{row.quantity}</TableCell>
+                <TableCell>
+                  <Button
+                    onClick={() => {
+                      cartContext.removeFromCart(row.itemName);
+                      cartContext.removeFromWidget(row.quantity);
+                      toast.info(
+                        `🦄 removed ${row.quantity} products from cart!`,
+                        {
+                          position: "top-center",
+                          autoClose: 2000,
+                          hideProgressBar: false,
+                          closeOnClick: true,
+                          pauseOnHover: true,
+                          draggable: true,
+                          progress: undefined,
+                          theme: "colored",
+                        }
+                      );
+                    }}
+                    variant="outlined"
+                    color="error"
+                  >
+                    Remove
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+            <TableRow>
+              <TableCell colSpan={2}>Total</TableCell>
               <TableCell align="right">
-                <Button
-                  onClick={() => {
-                    cartContext.removeFromCart(row.itemName);
-                    cartContext.removeFromWidget(row.quantity);
-                    toast.info(
-                      `🦄 removed ${row.quantity} products from cart!`,
-                      {
-                        position: "top-center",
-                        autoClose: 2000,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        pauseOnHover: true,
-                        draggable: true,
-                        progress: undefined,
-                        theme: "colored",
-                      }
-                    );
-                  }}
-                  variant="outlined"
-                  color="error"
-                >
-                  Remove
-                </Button>
+                &nbsp;$
+                {new Intl.NumberFormat("de-DE").format(
+                  cartContext.getTotalPrice()
+                )}
+              </TableCell>
+
+              <TableCell colSpan={2} align="right">
+                {cartContext.WidgetAmount} Items
               </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Checkout></Checkout>
+    </>
   );
 };
